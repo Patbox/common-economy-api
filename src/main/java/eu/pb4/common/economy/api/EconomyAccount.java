d87d61a3810a1e60ab106265c7af1128ce9f62cd
skycatminepokie <skycat@skycatdev.com>
2026-07-22 15:04:25 -0700

    Clarify account documentation
    Removed requirement that canIncrease/DecreaseBalance 
    returns a transaction with numerical values as if it
    had succeded - this conflicts with
    EconomyTransaction#finalBalance's documentation.

--- src/main/java/eu/pb4/common/economy/api/EconomyAccount.java
+++ src/main/java/eu/pb4/common/economy/api/EconomyAccount.java
@@ -7,36 +7,30 @@ import net.minecraft.world.item.ItemStack;
 import java.math.BigInteger;
 import java.util.UUID;
 
-@SuppressWarnings({"unused"})
+/// An account for holding currency. MAY have zero or more owners.
+@SuppressWarnings("unused")
 public interface EconomyAccount {
-    /**
-     * Accounts name, used for mods for a display
-     */
+    /// Account's name. SHOULD be used for mods for display purposes.
     Component name();
 
-    /**
-     * Main account owner. Return Util.NIL_UUID if it's server/console account or it doesn't have a main owner
-     */
+    /// Main account owner. MUST return [net.minecraft.util.Util.NIL_UUID] if it is a server/console account or doesn't
+    /// have a main owner.
     UUID owner();
 
-    /**
-     * Identifier allowing you to get this account. namespace should be equal to Provider's id.
-     * @return
-     */
+    /// Identifier for this account type. Namespace MUST be equal to provider's namespace.
     Identifier id();
 
-    /**
-     * Raw value of current account balance
-     */
+    /// Raw value of current account balance.
     BigInteger balance();
 
     default Component formattedBalance() {
         return this.currency().formatValueComponent(this.balance(), false);
     }
 
-    /**
-     * Increases account's balance by value. It should be only successful if full amount can be transferred
-     */
+    /// Increases account's balance by value.
+    ///
+    /// Returned transaction MUST be successful if full amount was added. Transaction MUST be unsuccessful otherwise.
+    /// There MUST NOT be any amount transferred for an unsuccessful transaction.
     default EconomyTransaction increaseBalance(BigInteger value) {
         var t = this.canIncreaseBalance(value);
 
@@ -50,19 +44,20 @@ public interface EconomyAccount {
         return increaseBalance(BigInteger.valueOf(value));
     }
 
-    /**
-         * Checks if account's balance can be increased by value. It should be only successful if full amount can be transferred.
-         * All returned values should be the same as for successful operation
-         */
+    /// Checks if account's balance can be increased by value.
+    ///
+    /// Returned transaction MUST be successful if full amount can be added. Transaction MUST be unsuccessful otherwise.
     EconomyTransaction canIncreaseBalance(BigInteger value);
     
     default EconomyTransaction canIncreaseBalance(long value) {
         return canIncreaseBalance(BigInteger.valueOf(value));
     }
 
-    /**
-     * Decreases account's balance by value. It should be only successful if full amount can be transferred
-     */
+    /// Decreases account's balance by value.
+    ///
+    /// Returned transaction MUST be successful if full amount can be removed.
+    /// Transaction MUST be unsuccessful otherwise. There MUST NOT be any amount transferred for an unsuccessful
+    /// transaction.
     default EconomyTransaction decreaseBalance(BigInteger value) {
         var t = this.canDecreaseBalance(value);
 
@@ -81,38 +76,33 @@ public interface EconomyAccount {
         return t;
     }
 
-    /**
-     * Checks if account's balance can be increased by value. It should be only successful if full amount can be transferred.
-     * All returned values should be the same as for successful operation
-     */
+    /// Checks if account's balance can be decreased by value.
+    ///
+    /// Returned transaction MUST be successful if full amount can be removed. Transaction MUST be unsuccessful
+    /// otherwise.
+    ///
+    /// If successful, a subsequent call to [#decreaseBalance(BigInteger)] with the same value MUST succeed, provided
+    /// the account is not externally modified between calls.
     EconomyTransaction canDecreaseBalance(BigInteger value);
     
     default EconomyTransaction canDecreaseBalance(long value) {
         return canDecreaseBalance(BigInteger.valueOf(value));
     }
 
-    /**
-     * Sets account's balance to a value
-     */
+    /// Sets account's balance to value.
     void setBalance(BigInteger value);
 
     default void setBalance(long value) {
         setBalance(BigInteger.valueOf(value));
     }
 
-    /**
-     * Provider managing this account
-     */
+    /// Provider managing this account.
     EconomyProvider provider();
 
-    /**
-     * Currency used by this account
-     */
+    /// Currency used by this account.
     EconomyCurrency currency();
 
-    /**
-     * Icon for other mods to use
-     */
+    /// An icon other mods SHOULD use.
     default ItemStack accountIcon() {
         return this.provider().icon();
     }
